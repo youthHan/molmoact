ae4d5f1acec4a4ba71473a785186f3c41f89fc74
hanmingfei <hanmingfei@bytedance.com>
2025-09-22 18:18:39 +0800

    unshard ckpt and to hf

--- scripts/unshard_ckpt.py
+++ scripts/unshard_ckpt.py
@@ -1,20 +1,21 @@
 from pathlib import Path
 import torch
 from olmo.train.distributed_checkpointing import unshard_checkpoint
-from olmo.train.checkpointer import Checkpointer, load_model_state_unsharded, load_model_state_hf, is_unsharded_checkpoint, is_hf_checkoint
+from olmo.train.checkpointer import Checkpointer, load_model_state_unsharded, load_model_state_hf, is_unsharded_checkpoint, is_hf_checkoint, save_unsharded
 from olmo.train.trainer_config import TrainConfig
 
-# src = Path("checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step6000/model_and_optim")          # sharded checkpoint
-# dst = src.with_name(src.name + "-unsharded")          # new output dir
-# unshard_checkpoint(src, dst, optim=False)             # set True if you also want optim.pt
+base_dir = "checkpoints/libero-goal-b100-40k-bs32x4-repl/step30000"
+src = Path(f"{base_dir}/model_and_optim")          # sharded checkpoint
+dst = Path(f"{base_dir}-unsharded")          # new output dir
+unshard_checkpoint(src, dst, optim=False)             # set True if you also want optim.pt
 
 from peft import LoraConfig, get_peft_model, PeftModel
 
-cfg: TrainConfig = TrainConfig.load("checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step6000/config.yaml")
+cfg: TrainConfig = TrainConfig.load(f"{base_dir}/config.yaml")
 model_cfg = cfg.model
 with torch.device("meta"):
-    olmo_model = model_cfg.build_model()                    # create Molmo
-olmo_model.to_empty(device=torch.device("cpu"))
+    base_model = model_cfg.build_model()                    # create Molmo
+base_model.to_empty(device=torch.device("cpu"))
 
 lora_cfg = LoraConfig(
                     r=model_cfg.lora_rank,
@@ -25,6 +26,31 @@ lora_cfg = LoraConfig(
                     bias=model_cfg.lora_bias,
                     init_lora_weights="gaussian",
                 )                  # same r/alpha/targets used in training
-model = get_peft_model(olmo_model, lora_cfg)
-load_model_state_unsharded("checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step6000-unsharded", model) # or load sharded checkpoint
-model = PeftModel.from_pretrained(model, "checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step6000-lora")
+peft_model = get_peft_model(base_model, lora_cfg)
+
+# load the PEFT-structured checkpoint
+load_model_state_unsharded(f"{base_dir}-unsharded", peft_model)
+
+# fuse LoRA into the base and drop the wrapper
+merged = peft_model.merge_and_unload()  # returns the plain Molmo module
+out_dir = f"{base_dir}-merged"
+save_unsharded(
+    out_dir,
+    merged,
+    optim=None,                  # pass your optimizer if you also want its state
+    config=cfg,                  # the TrainConfig (so config.yaml is written)
+    overwrite=True,
+)
+"""
+python3 -m olmo.hf_model.molmoact.convert_molmoact_to_hf  \
+    /mnt/bn/kinetics-lp-maliva/playground_projects/MolmoAct/checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step10000-merged \
+    /mnt/bn/kinetics-lp-maliva/playground_projects/MolmoAct/checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step10000-merged-hf \
+    demo_role \
+    --precision bf16
+
+python3 -m olmo.hf_model.molmoact.convert_molmoact_to_hf \
+    /mnt/bn/kinetics-lp-maliva/playground_projects/MolmoAct/checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step10000-merged \
+    /mnt/bn/kinetics-lp-maliva/playground_projects/MolmoAct/checkpoints/libero-all-b100-10k-bs256x4-wconnector-repl/step10000-merged-hf \
+    demo_role \
+    --precision bf16
+"""
